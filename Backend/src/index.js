8a0bc57b24880e8f0689e0b3cd3c0ea356b17eef
nishant-shekhar <nishant.shekhar10@gmail.com>
2025-01-22 12:08:21 +0530

    increased server limit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -29,7 +29,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json({ limit: '50mb' })); // Adjust the limit as needed
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 // (2) Define your API routes
--- Frontend/src/components/UserForm/Postseed.jsx
+++ Frontend/src/components/UserForm/Postseed.jsx
@@ -14,7 +14,7 @@ export const App = () => {
 	);
 };
 
-const PostSeed = ( {onFormSubmitSuccess} ) => {
+const PostSeed = ({ onFormSubmitSuccess }) => {
 	const [successMessage, setSuccessMessage] = useState("");
 	const [errorMessage, setErrorMessage] = useState("");
 	const [statusPopup, setStatusPopup] = useState(false);
@@ -32,18 +32,18 @@ const PostSeed = ( {onFormSubmitSuccess} ) => {
 		),
 		raisedFunds: Yup.string().required("Raised Funds is required"),
 		employment: Yup.string().required("Employment is required"),
-		
-			auditedBalanceSheet: Yup.mixed()
+
+		auditedBalanceSheet: Yup.mixed()
 			.test('fileRequired', 'Audited Balance Sheet is required', (value) => {
-			  return value && value instanceof File;
+				return value && value instanceof File;
 			}),
-		  gstReturn: Yup.mixed()
+		gstReturn: Yup.mixed()
 			.test('fileRequired', 'GST Return is required', (value) => {
-			  return value && value instanceof File;
+				return value && value instanceof File;
 			}),
-		  projectReport: Yup.mixed()
+		projectReport: Yup.mixed()
 			.test('fileRequired', 'Project Report is required', (value) => {
-			  return value && value instanceof File;
+				return value && value instanceof File;
 			}),
 	});
 	// Formik setup for handling form submission
@@ -118,243 +118,243 @@ const PostSeed = ( {onFormSubmitSuccess} ) => {
 		setDialogStatus({ ...dialogStatus, isVisible: false })
 		console.log("navigate to home")
 		onFormSubmitSuccess();
-	  }
-	
+	}
+
 
 	return (
 		<div className="h-screen overflow-y-auto">
-			 <div className="relative w-full h-[250px]">
+			<div className="relative w-full h-[250px]">
 
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="1440" height="250" preserveAspectRatio="none" viewBox="0 0 1440 250">
-  <g mask="url(#SvgjsMask1000)" fill="none">
-    <rect width="1440" height="250" x="0" y="0" fill="#0e2a47"></rect>
-    <path d="M38 250L288 0L538.5 0L288.5 250z" fill="url(#SvgjsLinearGradient1001)"></path>
-    <path d="M244.60000000000002 250L494.6 0L647.6 0L397.6 250z" fill="url(#SvgjsLinearGradient1001)"></path>
-    <path d="M490.20000000000005 250L740.2 0L911.2 0L661.2 250z" fill="url(#SvgjsLinearGradient1001)"></path>
-    <path d="M728.8000000000001 250L978.8000000000001 0L1289.3000000000002 0L1039.3000000000002 250z" fill="url(#SvgjsLinearGradient1001)"></path>
-    <path d="M1406 250L1156 0L982 0L1232 250z" fill="url(#SvgjsLinearGradient1002)"></path>
-    <path d="M1199.4 250L949.4000000000001 0L749.9000000000001 0L999.9000000000001 250z" fill="url(#SvgjsLinearGradient1002)"></path>
-    <path d="M940.8 250L690.8 0L375.79999999999995 0L625.8 250z" fill="url(#SvgjsLinearGradient1002)"></path>
-    <path d="M704.1999999999999 250L454.19999999999993 0L146.69999999999993 0L396.69999999999993 250z" fill="url(#SvgjsLinearGradient1002)"></path>
-    <path d="M1205.2767553797382 250L1440 15.276755379738262L1440 250z" fill="url(#SvgjsLinearGradient1001)"></path>
-    <path d="M0 250L234.72324462026174 250L 0 15.276755379738262z" fill="url(#SvgjsLinearGradient1002)"></path>
-  </g>
-  <defs>
-    <mask id="SvgjsMask1000">
-      <rect width="1440" height="250" fill="#ffffff"></rect>
-    </mask>
-    <linearGradient x1="0%" y1="100%" x2="100%" y2="0%" id="SvgjsLinearGradient1001">
-      <stop stop-color="rgba(15, 70, 185, 0.2)" offset="0"></stop>
-      <stop stop-opacity="0" stop-color="rgba(15, 70, 185, 0.2)" offset="0.66"></stop>
-    </linearGradient>
-    <linearGradient x1="100%" y1="100%" x2="0%" y2="0%" id="SvgjsLinearGradient1002">
-      <stop stop-color="rgba(15, 70, 185, 0.2)" offset="0"></stop>
-      <stop stop-opacity="0" stop-color="rgba(15, 70, 185, 0.2)" offset="0.66"></stop>
-    </linearGradient>
-  </defs>
-</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="1440" height="250" preserveAspectRatio="none" viewBox="0 0 1440 250">
+					<g mask="url(#SvgjsMask1000)" fill="none">
+						<rect width="1440" height="250" x="0" y="0" fill="#0e2a47"></rect>
+						<path d="M38 250L288 0L538.5 0L288.5 250z" fill="url(#SvgjsLinearGradient1001)"></path>
+						<path d="M244.60000000000002 250L494.6 0L647.6 0L397.6 250z" fill="url(#SvgjsLinearGradient1001)"></path>
+						<path d="M490.20000000000005 250L740.2 0L911.2 0L661.2 250z" fill="url(#SvgjsLinearGradient1001)"></path>
+						<path d="M728.8000000000001 250L978.8000000000001 0L1289.3000000000002 0L1039.3000000000002 250z" fill="url(#SvgjsLinearGradient1001)"></path>
+						<path d="M1406 250L1156 0L982 0L1232 250z" fill="url(#SvgjsLinearGradient1002)"></path>
+						<path d="M1199.4 250L949.4000000000001 0L749.9000000000001 0L999.9000000000001 250z" fill="url(#SvgjsLinearGradient1002)"></path>
+						<path d="M940.8 250L690.8 0L375.79999999999995 0L625.8 250z" fill="url(#SvgjsLinearGradient1002)"></path>
+						<path d="M704.1999999999999 250L454.19999999999993 0L146.69999999999993 0L396.69999999999993 250z" fill="url(#SvgjsLinearGradient1002)"></path>
+						<path d="M1205.2767553797382 250L1440 15.276755379738262L1440 250z" fill="url(#SvgjsLinearGradient1001)"></path>
+						<path d="M0 250L234.72324462026174 250L 0 15.276755379738262z" fill="url(#SvgjsLinearGradient1002)"></path>
+					</g>
+					<defs>
+						<mask id="SvgjsMask1000">
+							<rect width="1440" height="250" fill="#ffffff"></rect>
+						</mask>
+						<linearGradient x1="0%" y1="100%" x2="100%" y2="0%" id="SvgjsLinearGradient1001">
+							<stop stop-color="rgba(15, 70, 185, 0.2)" offset="0"></stop>
+							<stop stop-opacity="0" stop-color="rgba(15, 70, 185, 0.2)" offset="0.66"></stop>
+						</linearGradient>
+						<linearGradient x1="100%" y1="100%" x2="0%" y2="0%" id="SvgjsLinearGradient1002">
+							<stop stop-color="rgba(15, 70, 185, 0.2)" offset="0"></stop>
+							<stop stop-opacity="0" stop-color="rgba(15, 70, 185, 0.2)" offset="0.66"></stop>
+						</linearGradient>
+					</defs>
+				</svg>
 
 
-<div className="absolute top-9 left-0 w-full p-6 text-white">
-  <h1 className="text-3xl font-bold mb-2 relative top-10">Post Seed Form </h1>
-  <p className="text-lg max-w-xl relative top-10">
-    Share your innovative ideas and secure funding to turn them into reality.
-  </p>
-</div>
-</div>
+				<div className="absolute top-9 left-0 w-full p-6 text-white">
+					<h1 className="text-3xl font-bold mb-2 relative top-10">Post Seed Form </h1>
+					<p className="text-lg max-w-xl relative top-10">
+						Share your innovative ideas and secure funding to turn them into reality.
+					</p>
+				</div>
+			</div>
 
 			<div
 				className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
 				aria-hidden="true"
 			/>
-           
+
 			{/* Form Start */}
 			<form
 				onSubmit={formik.handleSubmit}
 				className=""
 			>
-				
-			<div className="bg-white p-6 border   border-black-500 rounded-md ">
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-				{/* Current Stage of Startup */}
-				
 
-				{/* Technical Knowledge */}
-				<div className="mb-6">
-					<label
-						htmlFor="currentStage"
-						className="block  text-sm/6 font-medium text-black  mb-2 break-words w-full max-w-[65%]"
-					>
-						Current stage of your 
-						
-						   Startup
-					</label>
-					<br />
-					<select
-						id="currentStage"
-						name="currentStage"
-						className="border  text-base text-gray-500 sm:text-sm/6   rounded w-3/4 py-2 px-3"
-						onChange={formik.handleChange}
-						value={formik.values.currentStage}
-					>
-						<option value="">Select Stage</option>
-						<option value="Ideation">Ideation</option>
-						<option value="Prototype">Prototype</option>
-						<option value="Validation">Validation</option>
-						<option value="MVP">MVP</option>
-						<option value="Scaling">Scaling</option>
-					</select>
-					{formik.errors.currentStage && (
-						<div className="text-red-600">{formik.errors.currentStage}</div>
-					)}
-				</div>
-				{/* Employment */}
-				<div className="mb-6">
-					<label
-						htmlFor="employment"
-						className="block  text-sm/6 font-medium text-black  mb-2 break-words w-full max-w-[65%]"
-					>
-						Has the startup given employment to 5-10 employees working
-						continuously for at least 6 months?
-					</label>
-					<select
-						id="employment"
-						name="employment"
-						className="border rounded  text-base text-gray-500 sm:text-sm/6  w-3/4 py-2 px-3"
-						onChange={formik.handleChange}
-						value={formik.values.employment}
-					>
-						<option value="">Select an option</option>
-						<option value="Yes">Yes</option>
-						<option value="No">No</option>
-					</select>
-					{formik.errors.employment && (
-						<div className="text-red-600">{formik.errors.employment}</div>
-					)}
-				</div>
-				<div className="mb-6">
-					<label
-						htmlFor="technicalKnowledge"
+				<div className="bg-white p-6 border   border-black-500 rounded-md ">
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+						{/* Current Stage of Startup */}
 
-						className="block  text-sm/6 font-medium text-black  mb-2 break-words w-full max-w-[65%]"
-					>
-						Do the founders/Key employees possess technical knowledge/necessary
-						skills to operate and scale the business?
-					</label>
-					<select
-						id="technicalKnowledge"
-						name="technicalKnowledge"
-						className="border rounded   text-base text-gray-500 sm:text-sm/6  w-3/4 py-2 px-3"
-						onChange={formik.handleChange}
-						value={formik.values.technicalKnowledge}
-					>
-						<option value="">Select an option</option>
-						<option value="Yes">Yes</option>
-						<option value="No">No</option>
-					</select>
-					{formik.errors.technicalKnowledge && (
-						<div className="text-red-600">
-							{formik.errors.technicalKnowledge}
+
+						{/* Technical Knowledge */}
+						<div className="mb-6">
+							<label
+								htmlFor="currentStage"
+								className="block  text-sm/6 font-medium text-black  mb-2 break-words w-full max-w-[65%]"
+							>
+								Current stage of your
+
+								Startup
+							</label>
+							<br />
+							<select
+								id="currentStage"
+								name="currentStage"
+								className="border  text-base text-gray-500 sm:text-sm/6   rounded w-3/4 py-2 px-3"
+								onChange={formik.handleChange}
+								value={formik.values.currentStage}
+							>
+								<option value="">Select Stage</option>
+								<option value="Ideation">Ideation</option>
+								<option value="Prototype">Prototype</option>
+								<option value="Validation">Validation</option>
+								<option value="MVP">MVP</option>
+								<option value="Scaling">Scaling</option>
+							</select>
+							{formik.errors.currentStage && (
+								<div className="text-red-600">{formik.errors.currentStage}</div>
+							)}
 						</div>
-					)}
-				</div>
-					
-				{/* Raised Funds */}
-				<div className="mb-6">
-					<label
-						htmlFor="raisedFunds"
-						className="block text-sm/6 font-medium text-black mb-2 break-words w-full max-w-[65%]"
-					>
-						Has the startup raised any fund/investment from recognized SEBI CAT
-						1 AIF, angel investors, or venture capitalists?
-					</label>
-					<select
-						id="raisedFunds"
-						name="raisedFunds"
-						className="border text-base text-gray-500 sm:text-sm/6 rounded w-3/4 py-2 px-3"
-						onChange={formik.handleChange}
-						value={formik.values.raisedFunds}
-					>
-						<option value="">Select an option</option>
-						<option value="Yes">Yes</option>
-						<option value="No">No</option>
-					</select>
-					{formik.errors.raisedFunds && (
-						<div className="text-red-600">{formik.errors.raisedFunds}</div>
-					)}
-				</div>
-				
-				</div>
-				<div class="border-b-2 botext-[#4b4b4b]/10 mb-8 pb-12"></div>
-				{/* File Upload: Audited Balance Sheet */}
-			 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-				<div className="mb-6">
-					<label
-						htmlFor="auditedBalanceSheet"
-						className="block text-sm/6 font-medium text-black   "
-					>
-						Upload Audited Balance Sheet:
-					</label>
-					<Upload
-						id="auditedBalanceSheet"
-						name="auditedBalanceSheet"
-						
-						onChange={(file) => formik.setFieldValue("auditedBalanceSheet", file)}
-					/>
-					{formik.errors.auditedBalanceSheet && (
-						<div className="text-red-600">
-							{formik.errors.auditedBalanceSheet}
+						{/* Employment */}
+						<div className="mb-6">
+							<label
+								htmlFor="employment"
+								className="block  text-sm/6 font-medium text-black  mb-2 break-words w-full max-w-[65%]"
+							>
+								Has the startup given employment to 5-10 employees working
+								continuously for at least 6 months?
+							</label>
+							<select
+								id="employment"
+								name="employment"
+								className="border rounded  text-base text-gray-500 sm:text-sm/6  w-3/4 py-2 px-3"
+								onChange={formik.handleChange}
+								value={formik.values.employment}
+							>
+								<option value="">Select an option</option>
+								<option value="Yes">Yes</option>
+								<option value="No">No</option>
+							</select>
+							{formik.errors.employment && (
+								<div className="text-red-600">{formik.errors.employment}</div>
+							)}
 						</div>
-					)}
-				</div>
+						<div className="mb-6">
+							<label
+								htmlFor="technicalKnowledge"
 
-				{/* File Upload: GST Return */}
-				<div className="mb-6">
-				<label htmlFor="gstReturn" className="block text-sm/6 font-medium text-black  ">
-                Upload GST Return: </label>
-					
-					<Upload
-						id="gstReturn"
-						name="gstReturn"
-						
-						
-						onChange={(file) => formik.setFieldValue("gstReturn", file)}
-					/>
-					{formik.errors.gstReturn && (
-						<div className="text-red-600">{formik.errors.gstReturn}</div>
-					)}
-				</div>
+								className="block  text-sm/6 font-medium text-black  mb-2 break-words w-full max-w-[65%]"
+							>
+								Do the founders/Key employees possess technical knowledge/necessary
+								skills to operate and scale the business?
+							</label>
+							<select
+								id="technicalKnowledge"
+								name="technicalKnowledge"
+								className="border rounded   text-base text-gray-500 sm:text-sm/6  w-3/4 py-2 px-3"
+								onChange={formik.handleChange}
+								value={formik.values.technicalKnowledge}
+							>
+								<option value="">Select an option</option>
+								<option value="Yes">Yes</option>
+								<option value="No">No</option>
+							</select>
+							{formik.errors.technicalKnowledge && (
+								<div className="text-red-600">
+									{formik.errors.technicalKnowledge}
+								</div>
+							)}
+						</div>
 
-				{/* File Upload: Project Report */}
-				<div className="mb-6">
-					<label
-						htmlFor="projectReport"
-						className="block text-sm/6 font-medium text-black  "
-					>
-						Upload Project Report:
-					</label>
-					<Upload
-						id="projectReport"
-						name="projectReport"
-						type="file"
-						onChange={(file) => formik.setFieldValue("projectReport", file)}
-				
-					/>
-					{formik.errors.projectReport && (
-						<div className="text-red-600">{formik.errors.projectReport}</div>
-					)}
-				</div>
-				</div>
-				{/* Submit Button */}
-				<div className="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                        <button 
-                            type="submit" 
-                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                           Submit
-                        </button>
-                    </div>
+						{/* Raised Funds */}
+						<div className="mb-6">
+							<label
+								htmlFor="raisedFunds"
+								className="block text-sm/6 font-medium text-black mb-2 break-words w-full max-w-[65%]"
+							>
+								Has the startup raised any fund/investment from recognized SEBI CAT
+								1 AIF, angel investors, or venture capitalists?
+							</label>
+							<select
+								id="raisedFunds"
+								name="raisedFunds"
+								className="border text-base text-gray-500 sm:text-sm/6 rounded w-3/4 py-2 px-3"
+								onChange={formik.handleChange}
+								value={formik.values.raisedFunds}
+							>
+								<option value="">Select an option</option>
+								<option value="Yes">Yes</option>
+								<option value="No">No</option>
+							</select>
+							{formik.errors.raisedFunds && (
+								<div className="text-red-600">{formik.errors.raisedFunds}</div>
+							)}
+						</div>
+
+					</div>
+					<div class="border-b-2 botext-[#4b4b4b]/10 mb-8 pb-12"></div>
+					{/* File Upload: Audited Balance Sheet */}
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+						<div className="mb-6">
+							<label
+								htmlFor="auditedBalanceSheet"
+								className="block text-sm/6 font-medium text-black   "
+							>
+								Upload Audited Balance Sheet:
+							</label>
+							<Upload
+								id="auditedBalanceSheet"
+								name="auditedBalanceSheet"
+
+								onChange={(file) => formik.setFieldValue("auditedBalanceSheet", file)}
+							/>
+							{formik.errors.auditedBalanceSheet && (
+								<div className="text-red-600">
+									{formik.errors.auditedBalanceSheet}
+								</div>
+							)}
+						</div>
+
+						{/* File Upload: GST Return */}
+						<div className="mb-6">
+							<label htmlFor="gstReturn" className="block text-sm/6 font-medium text-black  ">
+								Upload GST Return: </label>
+
+							<Upload
+								id="gstReturn"
+								name="gstReturn"
+
+
+								onChange={(file) => formik.setFieldValue("gstReturn", file)}
+							/>
+							{formik.errors.gstReturn && (
+								<div className="text-red-600">{formik.errors.gstReturn}</div>
+							)}
+						</div>
+
+						{/* File Upload: Project Report */}
+						<div className="mb-6">
+							<label
+								htmlFor="projectReport"
+								className="block text-sm/6 font-medium text-black  "
+							>
+								Upload Project Report:
+							</label>
+							<Upload
+								id="projectReport"
+								name="projectReport"
+								type="file"
+								onChange={(file) => formik.setFieldValue("projectReport", file)}
+
+							/>
+							{formik.errors.projectReport && (
+								<div className="text-red-600">{formik.errors.projectReport}</div>
+							)}
+						</div>
+					</div>
+					{/* Submit Button */}
+					<div className="mt-6 flex items-center justify-end gap-x-6">
+						<button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+						<button
+							type="submit"
+							className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						>
+							Submit
+						</button>
+					</div>
 
 
 				</div>
@@ -377,7 +377,7 @@ const PostSeed = ( {onFormSubmitSuccess} ) => {
 				onClose={() => goBacktoHome()}
 			/>
 		</div>
-		
+
 	);
 };
 
